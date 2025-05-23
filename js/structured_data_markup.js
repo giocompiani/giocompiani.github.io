85057da3d48c6049d90d32d2a9e9f71f73198f34
nathaniel-kren <kren.nathaniel@gmail.com>
2025-05-23 15:43:57 -0400

    SEO: added detailed meta tags (including Open Graph), produced SDML

---
+++ js/structured_data_markup.js
@@ -0,0 +1,229 @@
+[
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "Demand Estimate with Text and Image Data",
+    "author": [
+      {
+        "@type": "Person",
+        "name": "Giovanni Compiani"
+      },
+      {
+        "@type": "Person",
+        "name": "Ilya Morozov"
+      },
+      {
+        "@type": "Person",
+        "name": "Stephan Seiler"
+      }
+    ],
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg",
+    "articleBody": "abstract"
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "A Method to Estimate Discrete Choice Models that is Robust to Consumer Search",
+    "author": [
+      {
+        "@type": "Person",
+        "name": "Giovanni Compiani"
+      },
+      {
+        "@type": "Person",
+        "name": "Jason Abaluck"
+      },
+      {
+        "@type": "Person",
+        "name": "Fan Zhang"
+      }
+    ],
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg"
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "An Equilibrium Model of Rollover Lotteries",
+    "author": [
+      {
+        "@type": "Person",
+        "name": "Giovanni Compiani"
+      },
+      {
+        "@type": "Person",
+        "name": "Lorenzo Magnolfi"
+      },
+      {
+        "@type": "Person",
+        "name": "Lones Smith"
+      }
+    ],
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg"
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "When Cryptomining Comes to Town: High Electricity-Use Spillovers to the Local Economy",
+    "author": [
+      {
+        "@type": "Person",
+        "name": "Giovanni Compiani"
+      },
+      {
+        "@type": "Person",
+        "name": "Matteo Benetton"
+      },
+      {
+        "@type": "Person",
+        "name": "Adair Morse"
+      }
+    ],
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg"
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "Online Search and Optimal Product Rankings: An Empirical Framework",
+    "author": [
+      {
+        "@type": "Person",
+        "name": "Giovanni Compiani"
+      },
+      {
+        "@type": "Person",
+        "name": "Greg Lewis"
+      },
+      {
+        "@type": "Person",
+        "name": "Sida Peng"
+      },
+      {
+        "@type": "Person",
+        "name": "Will Wang"
+      }
+    ],
+    "datePublished": "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30",
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg"
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "Investors’ Beliefs and Cryptocurrency Prices",
+    "author": [
+      {
+        "@type": "Person",
+        "name": "Giovanni Compiani"
+      },
+      {
+      "@type": "Person",
+      "name": "Matteo Benetton"
+      }
+    ],
+    "datePublished": "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30",
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg"
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "An Instrumental Variable Approach to Dynamic Models",
+    "author": [
+      {
+        "@type": "Person",
+        "name": "Giovanni Compiani"
+      },
+      {
+      "@type": "Person",
+      "name": "Steven Berry"
+      }
+    ],
+    "datePublished": "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30",
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg"
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "Market Counterfactuals and the Specification of Multi-Product Demand: A Nonparametric Approach",
+    "author": {
+      "@type": "Person",
+      "name": "Giovanni Compiani"
+    },
+    "datePublished": "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30",
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg"
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "Time Periods Feel Longer When They Span More Category Boundaries: Evidence From the Lab and the Field",
+    "author": [
+      {
+        "@type": "Person",
+        "name": "Giovanni Compiani"
+      },
+      {
+        "@type": "Person",
+        "name": "Kristin Donnelly"
+      },
+      {
+        "@type": "Person",
+        "name": "Ellen Evers"
+      }
+    ],
+    "datePublished": "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30",
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg"
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "Empirical Models of Industry Dynamics with Endogenous Market Structure",
+    "author": [
+      {
+        "@type": "Person",
+        "name": "Giovanni Compiani"
+      },
+      {
+      "@type": "Person",
+      "name": "Steven Berry"
+      }
+    ],
+    "datePublished": "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30",
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg"
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "Common Values, Unobserved Heterogeneity, and Endogenous Entry in U.S. Offshore Oil Lease Auctions",
+    "author": [
+      {
+        "@type": "Person",
+        "name": "Giovanni Compiani"
+      },
+      {
+        "@type": "Person",
+        "name": "Philip Haile"
+      },
+      {
+        "@type": "Person",
+        "name": "Marcelo Sant’Anna"
+      }
+    ],
+    "datePublished": "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30",
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg"
+  },
+  {
+    "@context": "http://schema.org",
+    "@type": "Article",
+    "headline": "Using Mixtures in Econometric Models: a Brief Review and Some New Results",
+    "author": [
+      {
+        "@type": "Person",
+        "name": "Giovanni Compiani"
+      },
+      {
+      "@type": "Person",
+      "name": "Yuichi Kitamura"
+      }
+    ],
+    "datePublished": "Please insert valid ISO 8601 date/time here. Examples: 2015-07-27 or 2015-07-27T15:30",
+    "image": "https://giovannicompiani.com/images/portrait_giovanni_compiani.jpg"
+  }
+]
